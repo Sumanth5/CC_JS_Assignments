efaccf180e3bbfd5a54d92b1edb99916e933f9a8
Sumanth Subrahmanya <37635461+Sumanth5@users.noreply.github.com>
2020-11-20 19:39:24 -0500

    Create kelvin.js

---
+++ kelvin.js
@@ -0,0 +1,11 @@
+const kelvin = 283;
+//kelvinn = 293
+var celsius = kelvin - 273;
+// kelvin - 273 = celsius
+var newton = celsius * (33/100);
+//formula
+Math.floor(newton);
+//rounding
+console.log(`The temperature is ${newton} degrees Newton.`)
+
+
