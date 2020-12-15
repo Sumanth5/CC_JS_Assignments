914aec9b9e9809f308726481d7504fe238731ddc
Sumanth Subrahmanya <37635461+Sumanth5@users.noreply.github.com>
2020-12-15 17:56:24 -0500

    Update pianokeys.js

--- pianokeys.js
+++ pianokeys.js
@@ -1,3 +1,8 @@
+/*
+Piano Keys
+You’re a web developer who has been hired by a music education company. 
+This client wants you to create an interactive game to help their beginner-level piano students study. Create a piano player with DOM events in JavaScript!*/
+
 // The keys and notes variables store the piano keys
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
 const notes = [];
