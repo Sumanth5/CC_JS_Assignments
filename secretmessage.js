2c48983ae2995f5b9a14384aae978a1077f87a94
Sumanth Subrahmanya <37635461+Sumanth5@users.noreply.github.com>
2020-12-24 18:32:54 -0500

    Update secretmessage.js

--- secretmessage.js
+++ secretmessage.js
@@ -1,5 +1,6 @@
 
-let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 
+                     'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 
 secretMessage.pop();
 console.log(secretMessage.length);
