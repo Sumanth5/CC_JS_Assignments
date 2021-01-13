e5e33ac900ac3647dbbb6a9d84bcb2777a1468f5
Sumanth Subrahmanya <37635461+Sumanth5@users.noreply.github.com>
2021-01-12 23:35:21 -0500

    Update sd.js

--- sd.js
+++ sd.js
@@ -1,1 +1,12 @@
 
+const kelvin = 283;
+//kelvinn = 293
+var celsius = kelvin - 273;
+// kelvin - 273 = celsius
+var newton = celsius * (33/100);
+//formula
+Math.floor(newton);
+//rounding
+console.log(`The temperature is ${newton} degrees Newton.`)
+
+
